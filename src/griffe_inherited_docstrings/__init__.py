993e4e1e9a316e8b472df47c6568392605ca07f6
Timothée Mazzucotelli <pawamoy@pm.me>
2023-11-14 15:34:05 +0100

    feat: Implement extension

--- src/griffe_inherited_docstrings/__init__.py
+++ src/griffe_inherited_docstrings/__init__.py
@@ -5,4 +5,6 @@ Griffe extension for inheriting docstrings.
 
 from __future__ import annotations
 
-__all__: list[str] = []
+from griffe_inherited_docstrings.extension import InheritDocstringsExtension
+
+__all__: list[str] = ["InheritDocstringsExtension"]
