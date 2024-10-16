fe5759bc802a887d221ed8fc791a996a9d2d2d3f
Thomas Marwitz <thomasmarwitz3@gmail.com>
2024-10-16 15:18:42 +0200

    feat: Implement functionality according to things discussed in issue

--- src/griffe_inherited_docstrings/extension.py
+++ src/griffe_inherited_docstrings/extension.py
@@ -3,42 +3,118 @@
 from __future__ import annotations
 
 import contextlib
+from enum import Enum
 from typing import TYPE_CHECKING, Any
 
-from griffe import AliasResolutionError, Extension
+from griffe import AliasResolutionError, Class, Docstring, Extension
 
 if TYPE_CHECKING:
-    from griffe import Docstring, Module, Object
+    from griffe import Module, Object
+
+class DocstringInheritStrategy(str, Enum):
+    """Enumeration for the possible docstring inheritance strategies."""
+
+    if_not_present = "if_not_present"
+    """Inherit docstrings of members if not present. This takes the first docstring of a member according to the MRO."""
+    merge = "merge"
+    """Merge docstrings of members in parent classes with given docstring. This proceeds down the inheritance tree, going from general docstrings to more specific ones."""
 
 
-def _inherited_docstring(obj: Object) -> Docstring | None:
-    for parent_class in obj.parent.mro():  # type: ignore[union-attr]
-        try:
-            if docstring := parent_class.members[obj.name].docstring:
-                return docstring
-        except KeyError:
-            pass
-    return None
 
 
-def _inherit_docstrings(obj: Object) -> None:
-    if obj.is_module:
-        for member in obj.members.values():
-            if not member.is_alias:
-                with contextlib.suppress(AliasResolutionError):
-                    _inherit_docstrings(member)  # type: ignore[arg-type]
-    elif obj.is_class:
-        for member in obj.members.values():
-            if not member.is_alias:
-                if member.docstring is None and (inherited := _inherited_docstring(member)):  # type: ignore[arg-type]
-                    member.docstring = inherited
-                if member.is_class:
-                    _inherit_docstrings(member)  # type: ignore[arg-type]
 
 
 class InheritDocstringsExtension(Extension):
     """Griffe extension for inheriting docstrings."""
 
+    def __init__(self, merge_docstrings: bool = False) -> None:
+        super().__init__()
+
+        self.merge = merge_docstrings
+        self.merge_separator: str = "\n\n"
+        self.merge_cache = {}
+
     def on_package_loaded(self, *, pkg: Module, **kwargs: Any) -> None:  # noqa: ARG002
         """Inherit docstrings from parent classes once the whole package is loaded."""
-        _inherit_docstrings(pkg)
+        self.merge_cache.clear()
+        self._inherit_docstrings(pkg, self.merge, self.merge_separator)
+        self.merge_cache.clear()
+
+    def _construct_docstring(
+        self, obj: Object, merge: bool = False, merge_separator: str = "\n\n",
+    ) -> Docstring | None:
+        """Construct a docstring object according to the strategy.
+
+        Parameters:
+            name: The name of the member. Needed to lookup the member in the parent classes.
+            obj: The object that contains the member. Needed to access parent classes.
+            strategy: The strategy to use: default, if_not_present, merge.
+            merge_separator: The delimiter to use when merging docstrings.
+
+        Returns:
+            A new docstring object that just contains the docstring content.
+        """
+        name = obj.name
+
+        if not merge:
+            for parent_class in obj.parent.mro():  # type: ignore[union-attr]
+                if name in parent_class.members and (docstring := parent_class.members[obj.name].docstring):
+                    return docstring
+            return None
+
+
+        docstrings = []
+        traversal_order: list[Class | Object] = [obj.parent, *obj.parent.mro()]
+        # We traverse the parents in the order of the MRO, i.e. the first entry is the most direct parent
+        # We also include the object itself because we don't want to miss the docstring of the object itself if present
+        # In the end, the docstrings are merged in reverse order, to have the most general docstring come first
+        # We traverse in the order of the MRO to be able to break as soon as we find a previously merged docstring
+        # This is to avoid merging the same docstring multiple times
+
+        for parent_class in traversal_order: # type: ignore[assignment]
+            if (parent_class.name, name) in self.merge_cache:
+                docstrings.append(
+                    self.merge_cache[(parent_class.name, name)]
+                )
+                break
+
+
+            if name in parent_class.members and (docstring := parent_class.members[obj.name].docstring):
+                docstrings.append(docstring.value)
+
+        if not docstrings:
+            # This guarantees that no empty docstring is constructed for a member that shouldn't have one at all
+            return None
+
+        docstring = merge_separator.join(reversed(docstrings))
+        self.merge_cache[(obj.parent.name, name)] = docstring
+
+        return Docstring(docstring)
+
+
+    def _inherit_docstrings(self, obj: Object, merge: bool = False, merge_separator: str = "\n\n") -> None:
+        if obj.is_module:
+            for member in obj.members.values():
+                if not member.is_alias:
+                    with contextlib.suppress(AliasResolutionError):
+                        self._inherit_docstrings(member, merge=merge, merge_separator=merge_separator)  # type: ignore[arg-type]
+        elif obj.is_class:
+            for member in obj.members.values():
+                if member.is_alias:
+                    continue
+
+                if member.is_class:
+                    self._inherit_docstrings(member)  # type: ignore[arg-type]
+
+                docstring = self._construct_docstring(member, merge=merge, merge_separator=merge_separator)  # type: ignore[arg-type]
+
+                if not docstring:
+                    # if the docstring is None, we don't want to set it
+                    continue
+
+                if not member.docstring and not merge:
+                    member.docstring = docstring
+                elif merge:
+                    member.docstring = docstring
+
+
--- tests/test_extension.py
+++ tests/test_extension.py
@@ -2,26 +2,106 @@
 
 from __future__ import annotations
 
+from typing import Callable
+
+import pytest
 from griffe import Extensions, temporary_visited_package
 
 from griffe_inherited_docstrings import InheritDocstringsExtension
+from griffe_inherited_docstrings.extension import DocstringInheritStrategy
 
 
-def test_inherit_docstrings() -> None:
-    """Inherit docstrings from parent classes."""
+@pytest.fixture(params=[
+    (
+        "Method docstrings",
+        """
+        class Obj: # just to verify that additional parent classes don't affect the result
+            ...
+
+        class Base(Obj):
+            def base(self):
+                {docstring_base} # Without triple quotes so we can control between empty docstring and no docstring.
+                ...
+
+        class Main(Base):
+            def base(self):
+                {docstring_main}
+                ...
+
+        class Sub(Main):
+            def base(self):
+                {docstring_sub}
+                ...
+        """,
+        lambda package, class_: package[f"{class_}.base"].docstring,
+    ),
+    (
+        "Attribute docstrings",
+        """
+        class Obj:
+            ...
+
+        class Base(Obj):
+            attr: int
+            {docstring_base}
+
+        class Main(Base):
+            attr: int
+            {docstring_main}
+
+
+        class Sub(Main):
+            attr: int
+            {docstring_sub}
+        """,
+        lambda package, class_: package[class_].members["attr"].docstring,
+    ),
+
+])
+def content(request: pytest.FixtureRequest) -> tuple[str, str, Callable]:
+    return request.param
+
+@pytest.mark.parametrize(
+    ("merge_docstrings", "docstrings_list", "expected_docstrings_list"),
+    [
+        (
+            False,
+            ['"""base"""', '"""main"""', ""],
+            ["base", "main", "main"],
+        ),  # main: stays the same (no merge); sub: main is taken (not base)
+        (
+            True,
+            ['"""base"""', '"""main"""', ""],
+            ["base", "base\n\nmain", "base\n\nmain"],
+        ),  # main: is merged with base; sub: empty is merged with base\n\nmain (not base\n\nmain\n\n)
+        (
+            True,
+            ["", '"""main"""', '"""sub"""'],
+            [None, "main", "main\n\nsub"],
+        ),  # Base class has no docstring after merging (as opposed to an empty one)
+    ],
+)
+def test_inherit_docstrings(
+    merge_docstrings: DocstringInheritStrategy, docstrings_list: list[str], expected_docstrings_list: list[str], content: tuple[str, str, Callable]
+) -> None:
+    """Test the inheritance strategies of docstrings for members.
+
+    Parameters:
+        strategy: The docstring inheritance strategy to use.
+        docstrings_list: The list of docstrings for the base, main, and sub classes. Needs triple quotes.
+        expected_docstrings_list: The expected list of docstrings for the base, main, and sub classes. Just the content, i.e. without triple quotes. None for no docstring at all.
+    """
+    docstring_base, docstring_main, docstring_sub = docstrings_list
+
+    to_test, code, get_docstring = content
+
     with temporary_visited_package(
         "package",
-        modules={
-            "__init__.py": """
-                class Parent:
-                    def method(self):
-                        '''Docstring from parent method.'''
-
-                class Child(Parent):
-                    def method(self):
-                        ...
-            """,
-        },
-        extensions=Extensions(InheritDocstringsExtension()),
+        modules={"__init__.py": code.format(docstring_base=docstring_base, docstring_main=docstring_main, docstring_sub=docstring_sub)},
+        extensions=Extensions(InheritDocstringsExtension(merge_docstrings=merge_docstrings)),
     ) as package:
-        assert package["Child.method"].docstring.value == package["Parent.method"].docstring.value
+        classes = ["Base", "Main", "Sub"]
+        docstrings = [get_docstring(package, class_) for class_ in classes]
+        docstring_values = [docstring.value if docstring else None for docstring in docstrings]
+
+        assert docstring_values == expected_docstrings_list, f"Failed for merge='{merge_docstrings}' during testing '{to_test}'"
